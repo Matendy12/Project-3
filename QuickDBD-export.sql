ecccb250bf480cd14f5850907f4fdea27a9818c7
Bella Oshomuvwe <147251051+BellaBeautiful4092023@users.noreply.github.com>
2024-02-27 19:25:00 -0600

    intial

---
+++ QuickDBD-export.sql
@@ -0,0 +1,28 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "all_traffic" (
+    "index" int   NOT NULL,
+    "origin_port" int   NOT NULL,
+    "response_port" int   NOT NULL,
+    "photo" string   NOT NULL,
+    "service" string   NOT NULL,
+    "traffic_pattern" string   NOT NULL,
+    CONSTRAINT "pk_all_traffic" PRIMARY KEY (
+        "index"
+     )
+);
+
+CREATE TABLE "traffic_patterns" (
+    "index" int   NOT NULL,
+    "pattern" string   NOT NULL,
+    "attack?" string   NOT NULL,
+    CONSTRAINT "pk_traffic_patterns" PRIMARY KEY (
+        "index"
+     )
+);
+
+ALTER TABLE "all_traffic" ADD CONSTRAINT "fk_all_traffic_traffic_pattern" FOREIGN KEY("traffic_pattern")
+REFERENCES "traffic_patterns" ("pattern");
+
